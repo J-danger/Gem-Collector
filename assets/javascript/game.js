1d6ab8a49ba19510cb8805a8b2f82881f486f46f
Stephen Howe <Stevehowe10@gmail.com>
2019-09-11 16:29:11 -0600

    Game is completely functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,115 +1,127 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Generate random number > 10
 
     var random = Math.floor(Math.random() * 30 + 10);
     console.log(random);
     var wins = 0;
-    var losses= 0;
+    var losses = 0;
     var total = 0;
-       
+
+
 
     function randomNumber() {
-        document.querySelector("#random-number").innerHTML = random        
-      }
-      function currentTotal() {
-        document.querySelector("#score").innerHTML = total        
-      }
-      function winning() {
-        document.querySelector("#wins").innerHTML = "Wins: " + wins        
-      }
-      function losing() {
-        document.querySelector("#losses").innerHTML = "Losses: " + losses        
-      }
-      function reset(){
-		Random=Math.floor(Math.random()*101+19);
-		console.log(Random)
-		$('#random-Number').text(Random);
-		total = 0;
-		userScore=0;
-		$('#totalScore').text(userScore);
-	}
-  
+        document.querySelector("#random-number").innerHTML = "Goal: " + random;
+    }
+    function currentTotal() {
+        document.querySelector("#score").innerHTML =  total;
+    }
+    function winning() {
+        document.querySelector("#wins").innerHTML = "Wins: " + wins;
+    }
+    function losing() {
+        document.querySelector("#losses").innerHTML = "Losses: " + losses;
+    }
+    function playAgain() {
+        document.querySelector("#score").innerHTML = "Play Again!";
+    }
+    function resetLose() {
+        random = Math.floor(Math.random() * 30 + 10);        
+        $('#random-number').text( "Goal: " + random);
+        total = 0;
+        // alert("Try again!")
+        currentTotal();
+    }
+    function resetWin() {
+        random = Math.floor(Math.random() * 30 + 10);        
+        $('#random-number').text( "Goal: " + random);
+        total = 0;
+        // alert("You win! Play Again?")
+        currentTotal();
+        
+        
+    }
+
+
     // Generate gem numbers on click
     randomNumber();
-    $("#jewel-1").on("click", function() {
+    $("#jewel-1").on("click", function () {
         var number1 = Math.floor(Math.random() * 11 + 1);
         total = total + number1;
-        currentTotal();        
+        currentTotal();
         console.log(number1);
         console.log(total)
-        if (total === random){
+        if (total === random) {
             wins++;
             winning();
-            alert("yay")
-        } else if (total > random){
+            resetWin();
+            
+        } else if (total > random) {
             losses++;
             losing();
-            alert("lost")
+            resetLose();
 
         }
     });
-   
-    $("#jewel-2").on("click", function() {
+
+    $("#jewel-2").on("click", function () {
         var number2 = Math.floor(Math.random() * 11 + 1);
         total = total + number2;
-        currentTotal();        
+        currentTotal();
         console.log(number2);
         console.log(total)
-        if (total === random){
+        if (total === random) {
             wins++;
             winning();
-            reset();
-            alert("yay")
-        } else if (total > random){
+            resetWin();
+
+        } else if (total > random) {
             losses++;
             losing();
-            reset();
-            alert("lost")
+            resetLose();
+
 
         }
     });
-    $("#jewel-3").on("click", function() {
+    $("#jewel-3").on("click", function () {
         var number3 = Math.floor(Math.random() * 11 + 1);
         total = total + number3;
-        currentTotal();        
+        currentTotal();
         console.log(number3);
         console.log(total)
-        if (total === random){
+        if (total === random) {
             wins++;
             winning();
-            reset();
-            alert("yay")
-        } else if (total > random){
+            resetWin();
+
+
+        } else if (total > random) {
             losses++;
             losing();
-            reset();
-            alert("lost")
+            resetLose();
 
         }
     });
-    $("#jewel-4").on("click", function() {
+    $("#jewel-4").on("click", function () {
         var number4 = Math.floor(Math.random() * 11 + 1);
         total = total + number4;
-        currentTotal();        
+        currentTotal();
         console.log(number4);
-        console.log(total) 
-        if (total === random){
+        console.log(total)
+        if (total === random) {
             wins++;
             winning();
-            reset();
-            alert("yay")
-        } else if (total > random){
+            resetWin();
+
+
+
+        } else if (total > random) {
             losses++;
             losing();
-            reset();
-            alert("lost")
+            resetLose();
 
-        }      
+        }
     });
-
-
-
-
+  
+  
 });
-
