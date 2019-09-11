8430cff7a94ce43a20e07c63b0c82e2fbec298f7
Stephen Howe <Stevehowe10@gmail.com>
2019-09-10 20:15:48 -0600

    counters working properly. Still need a reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,16 @@ $(document).ready(function() {
     console.log(random);
     var wins = 0;
     var losses= 0;
-   
+    var total = 0;
+       
 
     function randomNumber() {
-        document.querySelector("#random-number").innerHTML = random
-        
+        document.querySelector("#random-number").innerHTML = random        
       }
+      function currentTotal() {
+        document.querySelector("#score").innerHTML = total        
+      }
+      
     //   function attempts() {
     //     document.querySelector("#guesses").innerHTML = "Guesses: " + guesses;
     //   }
@@ -19,23 +23,59 @@ $(document).ready(function() {
     randomNumber();
     $("#jewel-1").on("click", function() {
         var number1 = Math.floor(Math.random() * 11);
+        total = total + number1;
+        currentTotal();        
         console.log(number1);
+        console.log(total)
+        if (total === random){
+            alert("yay")
+        } else if (total > random){
+            alert("lost")
+
+        }
     });
+   
     $("#jewel-2").on("click", function() {
         var number2 = Math.floor(Math.random() * 11);
+        total = total + number2;
+        currentTotal();        
         console.log(number2);
-      
+        console.log(total)
+        if (total === random){
+            alert("yay")
+        } else if (total > random){
+            alert("lost")
+
+        }
     });
     $("#jewel-3").on("click", function() {
         var number3 = Math.floor(Math.random() * 11);
+        total = total + number3;
+        currentTotal();        
         console.log(number3);
-       
+        console.log(total)
+        if (total === random){
+            alert("yay")
+        } else if (total > random){
+            alert("lost")
+
+        }
     });
     $("#jewel-4").on("click", function() {
         var number4 = Math.floor(Math.random() * 11);
+        total = total + number4;
+        currentTotal();        
         console.log(number4);
-        
+        console.log(total) 
+        if (total === random){
+            alert("yay")
+        } else if (total > random){
+            alert("lost")
+
+        }       
     });
+
+
 
 
 });
